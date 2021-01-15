55c09a15082b926926a8dbde1759f204f9377d29
vaish <vaish@DESKTOP-7E1DO2U>
2021-01-15 15:18:39 +0530

    Welcome

---
+++ ConstructorcoreConcept/src/com/csi/constructor/ConstructorConcept.java
@@ -0,0 +1,24 @@
+package com.csi.constructor;
+
+class Employee {
+	int empId = 222;
+	String empName = "VEERA";
+	double empSalary = 9876577.98;
+
+	public Employee() {
+		System.out
+				.println("\n Employee Id:" + empId + "\n Employee Name:" + empName + "\n Employee Salary:" + empSalary);
+
+	}
+
+	public Employee(String empAddress, long empcontactNumber) {
+		System.out.println("\n Employee Address: " + empAddress + "\n Employee Contact Number:" + empcontactNumber);
+	}
+}
+
+public class ConstructorConcept {
+	public static void main(String[] args) {
+		Employee e1 = new Employee();
+		Employee e2 = new Employee("NASHIK", 9879887799L);
+	}
+}
